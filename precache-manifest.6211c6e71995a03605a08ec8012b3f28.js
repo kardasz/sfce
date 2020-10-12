2b1b580ea9b5c7234e13170259b3883a73fb0d46
Krzysztof Kardasz <krzysztof@kardasz.eu>
2020-10-12 21:07:01 +0200

    Updates

---
+++ precache-manifest.6211c6e71995a03605a08ec8012b3f28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b1fd45c0748d76b1831f9570481f32",
+    "url": "/sfce/index.html"
+  },
+  {
+    "revision": "0fef40171e67f55a948e",
+    "url": "/sfce/static/css/2.62ff64f5.chunk.css"
+  },
+  {
+    "revision": "332f5cedfa332c7330da",
+    "url": "/sfce/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "0fef40171e67f55a948e",
+    "url": "/sfce/static/js/2.c53af8bf.chunk.js"
+  },
+  {
+    "revision": "870267bc23303939d9ca31cdf878e7b3",
+    "url": "/sfce/static/js/2.c53af8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332f5cedfa332c7330da",
+    "url": "/sfce/static/js/main.8a6dc6d0.chunk.js"
+  },
+  {
+    "revision": "0777d2796225921e1396",
+    "url": "/sfce/static/js/runtime-main.92c078cc.js"
+  }
+]);
