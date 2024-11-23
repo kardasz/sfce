1ca5a8b24b318b210a8c333d92be71a5d4a86980
Krzysztof Kardasz <krzysztof@kardasz.eu>
2024-11-23 14:28:23 +0100

    Symfony 7.0 certification topics

--- src/features/dashboard/Welcome.js
+++ src/features/dashboard/Welcome.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Welcome () {
     return <h3>
-        This is a simple todo app to help you learn<br/> <a href="https://certification.symfony.com/">Symfony 5.0 certification topics</a>
+        This is a simple todo app to help you learn<br/> <a href="https://certification.symfony.com/">Symfony 7.0 certification topics</a>
     </h3>;
 }
